12e7150e34c320b365022f489425218ac3ccf409
Habib Muhammad <zihnimiftah12@gmail.com>
2025-07-07 22:57:25 +0700

    updated code

--- ESPNOW_MTO/ESPNOW-DHT11_DEV1/ESPNOW-DHT11_DEV1.ino
+++ ESPNOW_MTO/ESPNOW-DHT11_DEV1/ESPNOW-DHT11_DEV1.ino
@@ -8,30 +8,30 @@ typedef struct struct_message {                   // buat struct dengan nama 'st
 } struct_message;                                 // pengenalan struct
 struct_message myData;                            // buat objek struct dengan nama 'myData'
 
-uint8_t broadcastAddress[] = {0x48, 0xE7, 0x29, 0x6D, 0x86, 0x23}; // ALAMAT MAC PENERIMA
+uint8_t broadcastAddress[] = {0xA4, 0xCF, 0x12, 0xFC, 0xA3, 0xCC}; // ALAMAT MAC PENERIMA
 
 //================ DHT11 LIB&VARS ================//
 #include <DHT.h>                                  // masukkan library DHT
-DHT dht(2, DHT11);                                // buat objek sensor dengan nama DHT pada pin GPIO2              
+DHT dht(14, DHT11);                               // buat objek sensor dengan nama DHT pada pin GPIO14              
 
 //================== OTHER VARS ==================//
 unsigned long lastTime = 0;                       // variabel untuk menyimpan millis() sementara
-unsigned long timerDelay = 2000;                  // variabel yang mengontrol jeda setiap pengiriman
+unsigned long timerDelay = 500;                   // variabel yang mengontrol jeda setiap pengiriman
  
 //================== VOID SETUP ==================//
 void setup() {                                    // perulangan yang hanya akan dijalankan sekali
   Serial.begin(115200);                           // mulai komunikasi serial dengan kecepatan 115200
   dht.begin();                                    // nulai komunikasi dengan sensor DHT
-  
+  pinMode(LED_BUILTIN, OUTPUT);                   // gunakan led internal sebagai output untuk indikator
   WiFi.mode(WIFI_STA);                            // ganti mode wifi ESP menjadi mode Station
-
+  
   if (esp_now_init() != 0) {                      // pengecekan jika espnow gagal inisialisasi
     Serial.println("Error initializing ESP-NOW"); // tampilkan pesan ESPNOW gagal
     return;                                       // kembali lagi ke awal program
   }
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER); // atur peran sebsgai CONTROLLER(pengirim data sensor)
   esp_now_register_send_cb(OnDataSent);           // atur fungsi callback untuk data yang akan dikirimkan
-  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);   // tambahkan perangkat penerima data
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);// tambahkan perangkat penerima data
                  //(alamat MAC penerima,peran penerima,wifi ch1,tanpa enskripsi,panjang enskripsi 0)
 }
  
@@ -50,9 +50,11 @@ void loop() {                                     // perulangan yang akan dijala
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {// fungsi callback ketika data dikirimkan 
   Serial.print("Last Packet Send Status: ");      // tampilkan pada serial monitor
   if (sendStatus == 0){                           // jika status pengiriman berhasil
-    Serial.println("Delivery success");           // tampilkan pada serial monitor
+    Serial.println("Delivery success");           // tampilkan sukses pada serial monitor
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); //flip flop LED jika sukses
   }
   else{                                           // jika status pengiriman gagal
-    Serial.println("Delivery fail");              // tampilkan pada serial monitor
+    Serial.println("Delivery fail");              // tampilkan gagal pada serial monitor
+    digitalWrite(LED_BUILTIN, HIGH);              // matikan LED jika data gagal dikirim
   }
 }
